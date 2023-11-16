c0fbf4f6f35bb5d619a985369f8ca4932f2913c9
Pratham <pratham@pop-os.localdomain>
2023-11-16 19:27:49 +0530

    first server commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Musicana
-Music App
---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+require("express-async-errors"); //we dont need to use try catch error it will teak care it self
+const express = require("express");
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const app = express();
+
+connection();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/login", authRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`listning on port ${port}...`));
